459b40d010fa799da17b2aafce595fea169a1741
nghiavt1302 <vtnghia01.bchkl@gmail.com>
2023-06-24 23:19:33 +0700

    test

--- product-service/src/main/java/com/nghiavt/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/nghiavt/productservice/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.nghiavt.productservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,9 @@ public class ProductController {
     @GetMapping
     public String test() {
         return "GET test" + env.getProperty("local.server.port");
+    }
+    @PostMapping
+    public String test1() {
+        return "POST test" + env.getProperty("local.server.port");
     }
 }
