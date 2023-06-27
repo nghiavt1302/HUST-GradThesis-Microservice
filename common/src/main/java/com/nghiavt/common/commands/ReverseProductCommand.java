673ea80c753a2ba8b4b3485771bef02b3232d006
Nghia Vu Trong <nghiavt3@officevnpay.com>
2023-06-27 16:15:43 +0700

    Import common, saga hanlder

---
+++ common/src/main/java/com/nghiavt/common/commands/ReverseProductCommand.java
@@ -0,0 +1,15 @@
+package com.nghiavt.common.commands;
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class ReverseProductCommand {
+    @TargetAggregateIdentifier
+    private final String productId;
+    private final Integer quantity;
+    private final String orderId;
+    private final String userId;
+}
--- order-service/src/main/java/com/nghiavt/orderservice/saga/OrderSaga.java
+++ order-service/src/main/java/com/nghiavt/orderservice/saga/OrderSaga.java
@@ -1,11 +1,18 @@
 package com.nghiavt.orderservice.saga;
 
+import com.nghiavt.common.commands.ReverseProductCommand;
 import com.nghiavt.orderservice.core.event.OrderCreatedEvent;
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.CommandResultMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.Nonnull;
+
 
 @Saga
 public class OrderSaga {
@@ -15,5 +22,20 @@ public class OrderSaga {
     @StartSaga
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(OrderCreatedEvent orderCreatedEvent) {
+        ReverseProductCommand reverseProductCommand = ReverseProductCommand.builder()
+                .orderId(orderCreatedEvent.getOrderId())
+                .productId(orderCreatedEvent.getProductId())
+                .userId(orderCreatedEvent.getProductId())
+                .quantity(orderCreatedEvent.getQuantity())
+                .build();
+        commandGateway.send(reverseProductCommand, new CommandCallback<ReverseProductCommand, Object>() {
+            @Override
+            public void onResult(@Nonnull CommandMessage<? extends ReverseProductCommand> commandMessage,
+                                 @Nonnull CommandResultMessage<?> commandResultMessage) {
+                if (commandResultMessage.isExceptional()){
+
+                }
+            }
+        });
     }
 }
