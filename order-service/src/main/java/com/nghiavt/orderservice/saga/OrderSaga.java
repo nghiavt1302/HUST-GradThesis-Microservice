550e3c52ce0b8ce2dffd1ce599586b852e9de8b6
Nghia Vu Trong <nghiavt3@officevnpay.com>
2023-06-27 15:04:34 +0700

    init order saga

---
+++ order-service/src/main/java/com/nghiavt/orderservice/saga/OrderSaga.java
@@ -0,0 +1,19 @@
+package com.nghiavt.orderservice.saga;
+
+import com.nghiavt.orderservice.core.event.OrderCreatedEvent;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.spring.stereotype.Saga;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Saga
+public class OrderSaga {
+    @Autowired
+    private transient CommandGateway commandGateway;
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent orderCreatedEvent) {
+    }
+}
