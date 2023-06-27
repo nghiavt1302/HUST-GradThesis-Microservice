a340d11dda023ef9666d517845fe61471b25bad2
nghiavt1302 <vtnghia01.bchkl@gmail.com>
2023-06-27 10:42:32 +0700

    Init order service

---
+++ order-service/src/test/java/com/nghiavt/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nghiavt.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
